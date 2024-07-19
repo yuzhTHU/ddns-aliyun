9753f0b2b3ba92066f1a4ed09fe40fca0c82f03e
yuzh <yuzh19@mails.tsinghua.edu.cn>
2024-07-19 10:36:27 +0800

    bugfix in IPSB

--- ipv4/ipsb.py
+++ ipv4/ipsb.py
@@ -4,5 +4,5 @@ import ipv4
 
 class IPSB(ipv4.IPV4):
     def get_ip():
-        ipsb = ipv4.request.urlopen('https://api-ipv4.ip.sb/ip').read().decode('utf-8')
+        ipsb = ipv4.request.urlopen('https://api-ipv4.ip.sb/ip').read().decode('utf-8').strip()
         return ipsb
