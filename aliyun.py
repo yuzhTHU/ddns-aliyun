2d84bc5304d0921661df71fb08926f471e1949c5
yuzihan@RL3 <yumeow@126.com>
2024-08-08 11:42:58 +0800

    修改了 aliyun.py，主要是 CommonParams

--- aliyun.py
+++ aliyun.py
@@ -13,18 +13,19 @@ Headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36'
 }
 
-CommonParams = {
-    'Format':'json',
-    'SignatureMethod': 'HMAC-SHA1',
-    'SignatureVersion': '1.0',
-    'Timestamp': datetime.datetime.utcnow().isoformat(),
-    'Version': '2015-01-09',
-}
 
 class Aliyun():
     def __init__(self, access_key_id, access_key_secret):
         self.access_key_id = access_key_id
         self.access_key_secret = access_key_secret
+        self.params = {
+            'AccessKeyId': self.access_key_id,
+            'Format':'json',
+            'SignatureMethod': 'HMAC-SHA1',
+            'SignatureVersion': '1.0',
+            'Timestamp': datetime.datetime.utcnow().isoformat(),
+            'Version': '2015-01-09',
+        }
 
     
     def ddns(self, domain_name, ip, sub_domains):
@@ -46,11 +47,8 @@ class Aliyun():
 
 
     def check_domain_exists(self, domain_name):
-        CommonParams['AccessKeyId'] = self.access_key_id
-        CommonParams['Action'] = 'DescribeDomainInfo'
-        CommonParams['DomainName'] = domain_name
         try:
-            self._get_response_data(CommonParams)
+            self._get_response_data(Action='DescribeDomainInfo', DomainName=domain_name, **self.params)
             return True
         except Exception as e:
             logging.error(e)
@@ -58,11 +56,8 @@ class Aliyun():
 
 
     def get_record_value(self, domain_name, sub_domain, record_type):
-        CommonParams['AccessKeyId'] = self.access_key_id
-        CommonParams['Action'] = 'DescribeDomainRecords'
-        CommonParams['DomainName'] = domain_name
         try:
-            data = self._get_response_data(CommonParams)
+            data = self._get_response_data(Action='DescribeDomainRecords', DomainName=domain_name, **self.params)
             records = data['DomainRecords']['Record']
             for record in records:
                 if record['Type'] == record_type and record['RR'] == sub_domain:
@@ -75,11 +70,8 @@ class Aliyun():
 
 
     def get_record_id(self, domain_name, sub_domain, record_type):
-        CommonParams['AccessKeyId'] = self.access_key_id
-        CommonParams['Action'] = 'DescribeDomainRecords'
-        CommonParams['DomainName'] = domain_name
         try:
-            data = self._get_response_data(CommonParams)
+            data = self._get_response_data(Action='DescribeDomainRecords', DomainName=domain_name, **self.params)
             records = data['DomainRecords']['Record']
             for record in records:
                 if record['Type'] == record_type and record['RR'] == sub_domain:
@@ -92,15 +84,8 @@ class Aliyun():
 
 
     def add_record(self, domain_name, sub_domain, record_type, localIP):
-        CommonParams['AccessKeyId'] = self.access_key_id
-        CommonParams['Action'] = 'AddDomainRecord'
-        CommonParams['DomainName'] = domain_name
-        CommonParams['RR'] = sub_domain
-        CommonParams['Type'] = record_type
-        CommonParams['Value'] = localIP
-
         try:
-            data = self._get_response_data(CommonParams)
+            data = self._get_response_data(Action='AddDomainRecord', DomainName=domain_name, RR=sub_domain, Type=record_type, Value=localIP, **self.params)
             return data['RecordId']
         except Exception as e:
             logging.error(e)
@@ -109,27 +94,19 @@ class Aliyun():
 
     def update_record(self, domain_name, sub_domain, record_type, localIP):
         record_id = self.get_record_id(domain_name, sub_domain, record_type)
-        CommonParams['AccessKeyId'] = self.access_key_id
-        CommonParams['Action'] = 'UpdateDomainRecord'
-        CommonParams['RR'] = sub_domain
-        CommonParams['RecordId'] = record_id
-        CommonParams['Type'] = record_type
-        CommonParams['Value'] = localIP
-
         try:
-            data = self._get_response_data(CommonParams)
+            data = self._get_response_data(Action='UpdateDomainRecord', RR=sub_domain, RecordId=record_id, Type=record_type, Value=localIP, **self.params)
             return data['RecordId']
         except Exception as e:
             logging.error(e)
             raise Exception("Update record failed.")
 
 
-    def _get_response_data(self, params):
-        CommonParams['SignatureNonce'] = uuid.uuid1()
+    def _get_response_data(self, **params):
+        params['SignatureNonce'] = uuid.uuid1()
         params = self._sort_dict(params)
 
         params['Signature'] = self._sign(params)
-        print(params)
         req = request.Request(url=f'https://alidns.aliyuncs.com/?{parse.urlencode(params)}', headers=Headers, method='GET')
         response = request.urlopen(req)
         return json.loads(response.read().decode('utf-8'))
